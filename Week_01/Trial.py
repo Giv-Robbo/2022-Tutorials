804caed54cb68e3cf52a7904e22f018046a9388c
Giv-Robbo <giverny.robinson@uqconnect.edu.au>
2022-07-27 13:45:54 +1000

    Trying to do the first tutorial

--- Week_01/Trial.py
+++ Week_01/Trial.py
@@ -1,8 +1,23 @@
-# -*- coding: utf-8 -*-
 """
 Created on Wed Jul 27 13:25:00 2022
 
-@author: HP
-"""
+@author: Giv
 
-Print("Trying to work github")
+Exercise 1 from week 1 ICT1
+"""
+import numpy as np
+
+def f1(x):
+    return x**3
+
+def f2(x):
+    return 3*x**2-2*x
+
+def f3(x):
+    return np.sin(x)
+    
+def forward_diff(f,x,h):
+    return (f(x+h)-f(x))/h
+
+x=np.array([-5,-4,5])
+h=np.array([0.1,0.0001,0.0001])
